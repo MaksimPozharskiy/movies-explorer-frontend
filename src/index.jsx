828650ea34ce0fb9075fdb691a930075cae711fc
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2021-02-23 19:44:04 +0300

    feat: import normilize

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import "./vendor/normalize.css"
 import "./index.css";
 import App from "./components/App/App";
 
