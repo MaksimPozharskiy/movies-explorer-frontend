99c53a6c6093af5f8f1a27283a43746b174a9498
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2021-02-23 12:58:34 +0300

    refactor: добавить ссылки в Portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,22 +7,28 @@ function Portfolio() {
       <h4 className="portfolio__title">Портфолио</h4>
       <ul className="portfolio__projects">
         <li className="portfolio__project">
-          <p className="portfolio__name">Статичный сайт</p>
-          <svg className="portfolio__icon" width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="black" />
-          </svg>
+          <a className="portfolio__link" href="https://github.com/MaksimPozharskiy/how-to-learn">
+            <p className="portfolio__name">Статичный сайт</p>
+            <svg className="portfolio__icon" width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="black" />
+            </svg>
+          </a>
         </li>
         <li className="portfolio__project">
-          <p className="portfolio__name">Адаптивный сайт</p>
-          <svg className="portfolio__icon" width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="black" />
-          </svg>
+          <a className="portfolio__link" href="https://github.com/MaksimPozharskiy/russian-travel">
+            <p className="portfolio__name">Адаптивный сайт</p>
+            <svg className="portfolio__icon" width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="black" />
+            </svg>
+          </a>
         </li>
         <li className="portfolio__project">
-          <p className="portfolio__name">Одностраничное приложение</p>
-          <svg className="portfolio__icon" width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="black" />
-          </svg>
+          <a className="portfolio__link" href="https://github.com/MaksimPozharskiy/react-mesto-api-full">
+            <p className="portfolio__name">Одностраничное приложение</p>
+            <svg className="portfolio__icon" width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="black" />
+            </svg>
+          </a>
         </li>
       </ul>
     </div>
