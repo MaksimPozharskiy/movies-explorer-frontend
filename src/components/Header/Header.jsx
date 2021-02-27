47b98e759208a2a2e1482fa17fbe44d2a3e08ab8
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2021-02-28 00:00:54 +0300

    feat: сверстать активное бургер меню

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -36,11 +36,43 @@ function Header({ bgColor, textColor }) {
           )}
         </div>
         {pathname === "/" ? '' : (
-          <div className={`header__burger ${activeBurger ? 'header__burger_active' : ''}`} onClick={handleActiveBurger}>
-            <div className="header__burger-line" />
-            <div className="header__burger-line" />
-            <div className="header__burger-line" />
-          </div>
+          <>
+            <div className={`header__burger ${activeBurger ? 'header__burger_active' : ''}`} onClick={handleActiveBurger}>
+              <div className="header__burger-line" />
+              <div className="header__burger-line" />
+              <div className="header__burger-line" />
+            </div>
+            <div className={`header__burger-menu-wrap ${activeBurger ? 'header__burger-menu-wrap_active' : ''}`} onClick={handleActiveBurger}>
+              <nav className="header__burger-nav">
+                <ul className="header__burger-list">
+                  <li className="header__burger-item">
+                    <Link className="header__burger-link" to="/">Главная</Link>
+                  </li>
+                  <li className="header__burger-item">
+                    <Link className="header__burger-link" to="/movies">Фильмы</Link>
+                  </li>
+                  <li className="header__burger-item">
+                    <Link className="header__burger-link" to="/saved-movies">Сохранённые фильмы</Link>
+                  </li>
+                </ul>
+              </nav>
+              <div className={`header__wrapper header__wrapper_burger-menu ${pathname === "/" ? "header__wrapper_burger" : ""}`}>
+                <Link
+                  className={`header__sign-text header__sign-text_color_${textColor}`}
+                  to={`${pathname === "/" ? "/signup" : "/profile"}`}
+                >
+                  {text}
+                </Link>
+                {pathname === "/" ? (
+                  <Link to="/signin" className="header__btn-signin" type="button">
+                    Войти
+                  </Link>
+                    ) : (
+                      <button className="header__btn-account" type="button" />
+                    )}
+              </div>
+            </div>
+          </>
         )}
         
       </div>
