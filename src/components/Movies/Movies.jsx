dfba5344a9bece29de95c10ed135e4887fb4eefd
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2021-03-08 16:01:37 +0300

    feat: написать функционал поиска и отрисовки фильмов

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,15 +3,74 @@ import "./Movies.css";
 import SearchForm from "../SearchForm/SearchForm";
 import Preloader from "../Preloader/Preloader";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
-
-
+import MoviesApi from "../../utils/MoviesApi";
+import { definitionSizeScreen, coefficientScreen } from "../../utils/definitionScreen";
 
 function Movies() {
+  const [movies, setMoviesList] = React.useState([]); // Стейт с найденными по ключевому слову фильмами
+  const [renderedFilms, setRenderedFilms] = React.useState([]); // Отрисованные карточки
+  const [countClickMoreFilms, setCountClickMoreFilms] = React.useState(1); // Счетчик нажатий кнопки "еще"
+  const [searchValue, setSearchValue] = React.useState('');
+  const [inputError, setInputError] = React.useState('');
+  const [visibilityMoviesList, setVisibilityMoviesList] = React.useState('');
+
+  // Считаем сколько карточек нужно отрисовать при поиске
+  function countInitCards() {
+    const width = definitionSizeScreen();
+    if (width >= 1280) {
+      return 12;
+    } if (width >= 757) {
+      return 8;
+    } return 5;
+  }
+
+  // Добавляем фильмы по клику на кнопку "Ещё"
+  function handleMoreRenderFilms() {
+    const cards = countInitCards();
+    // Отрисовываем текущие + новые(в зависимости от ширины экрана)
+    setRenderedFilms(movies.slice(0, cards + countClickMoreFilms * coefficientScreen()))
+    setCountClickMoreFilms(countClickMoreFilms+1)
+  }
+
+  // Фильруем фильмы по ключевому слову
+  function filterMoviesFromLS(moviesList) {
+    setMoviesList(moviesList.filter(movie => movie.nameRU.includes(searchValue)))
+  }
+  // Получаем фильмов по ключевому слову по клику на Поиск
+  function handleSearch(evt) {
+    evt.preventDefault();
+
+    if (searchValue === '') {
+      setInputError('Нужно ввести ключевое слово')
+      return;
+    }
+
+    MoviesApi.getMovies()
+      .then(moviesList => {
+        localStorage.setItem('moviesList', JSON.stringify(moviesList));
+      })
+    filterMoviesFromLS(JSON.parse(localStorage.moviesList))
+    setVisibilityMoviesList('movies_visibility')
+  }
+
   return (
     <>
-      <SearchForm />
+      <SearchForm
+        onSubmit={handleSearch}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        inputError={inputError}
+        setInputError={setInputError}
+      />
       <Preloader />
-      <MoviesCardList />
+      <MoviesCardList 
+        movies={movies}
+        visibilityMoviesList={visibilityMoviesList}
+        renderedFilms={renderedFilms}
+        setRenderedFilms={setRenderedFilms}
+        handleMoreRenderFilms={handleMoreRenderFilms}
+        countInitCards={countInitCards}
+      />
     </>
   );
 }
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -2,17 +2,39 @@ import React from "react";
 import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard";
 
-function MoviesCardList() {
+
+function MoviesCardList({ movies, visibilityMoviesList, renderedFilms, setRenderedFilms, handleMoreRenderFilms, countInitCards }) {
+  React.useEffect(() => {
+    // Определяем сколько фильмов нужно отрисовать в зависимости от ширины экрана
+    const cards = countInitCards();
+    setRenderedFilms(movies.slice(0, cards));
+  }, [movies, setRenderedFilms])
+
+  // Расчёт длительности фильма
+    function parseDurationMovie(durationMinutes) {
+    const hours = Math.floor(durationMinutes / 60);
+    const minutes = durationMinutes % 60;
+    const durationHours = `${hours}ч ${minutes}м`
+    return durationHours;
+  }
+
   return (
-    <section className="movies">
-      <p className="movies__not-found">Ничего не найдено</p>
+    <section className={`movies ${visibilityMoviesList}`}>
+      {movies.length > 0 ? '' : <p className="movies__not-found">Ничего не найдено</p>}
       <ul className="movies__list">
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
+        
+        { 
+          renderedFilms.map((movie) => (
+            <MoviesCard
+              key={movie.id}
+              cardName={movie.nameRU}
+              cardDuration={parseDurationMovie(movie.duration)}
+              imageLink={movie.image ? `https://api.nomoreparties.co${movie.image.url}` : "https://thumbnailer.mixcloud.com/unsafe/900x900/extaudio/c/e/e/5/95df-f97e-4e8b-a1d5-94f3ceb4f5ea"}
+            />
+          )) 
+        }
       </ul>
-      <button className="movies__button" type="button">Ещё</button>
+      {movies.length > renderedFilms.length ? <button className="movies__button" type="button" onClick={handleMoreRenderFilms}>Ещё</button> : ''}
     </section>
   );
 }
